fcea1f86cfc2f46a443fd07a71c5d20c166db543
Catiflam <gilfrid1@gmail.com>
2023-08-31 15:02:11 +0200

    edit-todo-list-insert

--- js/main.js
+++ js/main.js
@@ -3,7 +3,34 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			message: "To do list",
+			todoList: [
+				{
+					text: "fare la spesa",
+					done: false,
+				},
+				{
+					text: "cucinare",
+					done: false,
+				},
+				{
+					text: "fare l'esercizio",
+					done: false,
+				},
+				{
+					text: "allenarsi",
+					done: false,
+				},
+			],
+			newTask: {
+				text: "",
+				done: false,
+			},
 		};
+	},
+	methods: {
+		addNewTask() {
+			this.todoList.push(this.newTask);
+		},
 	},
 }).mount("#app");
